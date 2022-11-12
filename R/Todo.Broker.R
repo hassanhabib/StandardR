89a506c1fed19b3c082e67e30616cdfb6d8dfb42
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-12 12:09:25 -0800

    BROKER: CreateNewTodoInStorage -> PASS

--- R/Todo.Broker.R
+++ R/Todo.Broker.R
@@ -2,7 +2,7 @@ Todo.Broker <- \(storage){
     operations <- list()
 
     operations[['Create']] <- \(todo){
-        
+        todo |> storage[['Todo']][['Insert']]()
     }
 
     return(operations)
