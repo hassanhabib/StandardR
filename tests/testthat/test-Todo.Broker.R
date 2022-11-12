7db96751fb70a0b98ddb32c4e24c7ddc933b79a4
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-12 10:38:18 -0800

    BROKER: ReturnsListOfOperations -> FAIL

--- tests/testthat/test-Todo.Broker.R
+++ tests/testthat/test-Todo.Broker.R
@@ -3,3 +3,20 @@ test_that("Todo.Broker Exist",{
     is.null() |>
       expect_equal(FALSE)
 })
+
+test_that("configuration |> Todo.Broker() returns a list of operations",{
+  # Given
+  configuration <- data.frame()
+  
+  storage <- 
+    configuration |> Storage::Mock.Storage.Service()
+
+  # When
+  todo.broker <- 
+    storage |> Todo.Broker() 
+  
+  # Then
+  todo.broker |>
+    is.list() |>
+      expect_equal(TRUE)
+})
