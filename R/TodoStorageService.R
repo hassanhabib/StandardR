e1209e45da9d4705ce41c2599a83988b021b45ac
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-12 10:10:17 -0800

    CODE RUB: Remove legacy Broker and Service

--- R/TodoStorageService.R
+++
@@ -1,9 +0,0 @@
-TodoStorageService <- \(broker) {
-  service <- list()
-  service[["AddTodo"]]    <- \(todo) todo |> broker[["CreateTodo"]]()
-  service[["GetTodos"]]   <- \()             broker[["RetrieveTodos"]]()
-  service[["UpdateTodo"]] <- \(todo) todo |> broker[["UpdateTodo"]]()
-  service[["DeleteTodo"]] <- \(id)     id |> broker[["DeleteTodo"]]()
-
-  return(service)
-}
