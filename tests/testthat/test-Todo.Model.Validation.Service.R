c802c820994b2d7c9ebd45ee49f29169d2708590
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:19:02 -0800

    FOUNDATIONS: ReturnsListOfValidators -> FAIL

--- tests/testthat/test-Todo.Model.Validation.Service.R
+++ tests/testthat/test-Todo.Model.Validation.Service.R
@@ -3,3 +3,13 @@ test_that("Todo.Model.Validation.Service Exist",{
     is.null() |>
       expect_equal(FALSE)
 })
+
+test_that("Todo.Model.Validation.Service() returns a list of validators",{
+  # Given
+  validators <- Todo.Model.Validation.Service()
+  
+  # Then
+  validators |>
+    is.list() |>
+      expect_equal(TRUE)
+})
