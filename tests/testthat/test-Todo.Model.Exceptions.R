b21a103b691e5c25ad9d54627c71c59558b8f3a0
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:30:58 -0800

    FOUNDATIONS: TodoIsNullExceptionWorks -> FAIL

--- tests/testthat/test-Todo.Model.Exceptions.R
+++ tests/testthat/test-Todo.Model.Exceptions.R
@@ -13,3 +13,35 @@ test_that("Todo.Model.Exceptions() returns a list of exceptions",{
     is.list() |>
       expect_equal(TRUE)
 })
+
+test_that("todo.model.exceptions instance has TodoIsNull exception",{
+    # Given
+    exception <- Todo.Model.Exceptions()
+
+    # Then
+    exception[["TodoIsNull"]]  |>
+      is.null()             |>
+        expect_equal(FALSE)
+})
+
+test_that("FALSE |> exception[['TodoIsNull']]() should not throw error",{
+    # Given
+    exception <- Todo.Model.Exceptions()
+
+    # Then
+    FALSE |>
+      exception[["TodoIsNull"]]() |>
+        expect_no_error()
+})
+
+test_that("TRUE |> exception[['TodoIsNull']]() should throw TodoIsNull error",{
+    # Given
+    exception <- Todo.Model.Exceptions()
+
+    error <- 'successful validation requires a data frame with todo'
+
+    # Then
+    TRUE |>
+      exception[["TodoIsNull"]]() |>
+        expect_error(error)
+})
