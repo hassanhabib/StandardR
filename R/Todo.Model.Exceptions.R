4fa86fa68cc8b8b4cd5907ee1ffe2870af2fc2e8
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 04:38:56 -0800

    FOUNDATIONS: IdIsNullExceptionWorks -> PASS

--- R/Todo.Model.Exceptions.R
+++ R/Todo.Model.Exceptions.R
@@ -18,6 +18,10 @@ Todo.Model.Exceptions <- \(){
     if (invoke) { stop("todo data frame has no Status")}
   }
 
+  exceptions[["IdIsNull"]] <- \(invoke) {
+    if (invoke) { stop("successful validation requires an Id")}
+  }
+
   # Logical Validation Exceptions
   exceptions[["DuplicateKey"]]  <- \(invoke) {
     if (invoke) { stop("todo already exist, duplicate key not allowed") }
