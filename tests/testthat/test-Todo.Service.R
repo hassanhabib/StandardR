27e93606c15dac4688b4bcd68eaebc065445814d
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:24:12 -0800

    FOUNDATIONS: ReturnsListOfService -> FAIL

--- tests/testthat/test-Todo.Service.R
+++ tests/testthat/test-Todo.Service.R
@@ -3,3 +3,22 @@ test_that("Todo.Service Exist",{
     is.null() |>
       expect_equal(FALSE)
 })
+
+test_that("broker |> Todo.Service() returns a list of services",{
+# Given
+  configuration <- data.frame()
+  
+  storage <- 
+    configuration |> Storage::Mock.Storage.Service()    
+  
+  todo.broker <-
+    storage |> Todo.Broker()     
+        
+  services <- 
+    todo.broker |> Todo.Service()
+
+  # Then
+  services |>
+    is.list() |>
+      expect_equal(TRUE)
+})
