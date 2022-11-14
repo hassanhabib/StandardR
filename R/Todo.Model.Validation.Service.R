5516cccd87f08cbc4a68ab1576089664e57f4614
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:49:14 -0800

    FOUNDATIONS: HasIDValidationWorks <- PASS

--- R/Todo.Model.Validation.Service.R
+++ R/Todo.Model.Validation.Service.R
@@ -8,5 +8,10 @@ Todo.Model.Validation.Service <- \(){
     return(todo)
   }
 
+  validators[["HasId"]]     <- \(todo) {
+    todo[["Id"]] |> is.null() |> exception[["IdIsNull"]]()
+    return(todo)
+  }
+
   return(validators)  
 }
