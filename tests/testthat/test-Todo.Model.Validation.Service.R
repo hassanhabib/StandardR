fcf030095f08a7a36eab1eb81d3d42d8295d503b
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:37:27 -0800

    FOUNDATIONS: TodoExistValidation Works -> FAIL

--- tests/testthat/test-Todo.Model.Validation.Service.R
+++ tests/testthat/test-Todo.Model.Validation.Service.R
@@ -13,3 +13,38 @@ test_that("Todo.Model.Validation.Service() returns a list of validators",{
     is.list() |>
       expect_equal(TRUE)
 })
+
+test_that("todo.model.validation.service instance has TodoExist validator",{
+    # Given
+    validator <- Todo.Model.Validation.Service()
+
+    # Then
+    validator[['TodoExist']] |>
+      is.null() |>
+        expect_equal(FALSE)
+})
+
+test_that("todo |> validate[['TodoExist']]() should not throw error if todo exist",{
+    # Given
+    validate <- Todo.Model.Validation.Service()
+
+    todo  <- data.frame()
+    
+    # Then
+    todo |> 
+      validate[['TodoExist']]() |>
+        expect_no_error()
+})
+
+test_that("todo |> validate[['TodoExist']]() should throw error if todo is null",{
+    # Given
+    validate <- Todo.Model.Validation.Service()
+
+    todo  <- NULL
+    error <- "successful validation requires a data frame with todo"
+
+    # Then
+    todo |> 
+      validate[['TodoExist']]() |>
+        expect_error(error)
+})
