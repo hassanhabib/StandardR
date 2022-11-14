c6aa9da5877532ab12a74db59fb299f58698eb3b
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:32:53 -0800

    FOUNDATIONS: TodoIsNullException Works -> PASS

--- R/Todo.Model.Exceptions.R
+++ R/Todo.Model.Exceptions.R
@@ -1,5 +1,10 @@
 Todo.Model.Exceptions <- \(){
   exceptions <- list()
+
+  # Structural Validation Exceptions
+  exceptions[["TodoIsNull"]]   <- \(invoke) {
+    if (invoke) { stop("successful validation requires a data frame with todo")}
+  }
   
   return(exceptions)
 }
