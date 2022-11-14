b8ea6338860fd71598a86655c752665cfc444d1a
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-14 01:40:45 -0800

    FOUNDATIONS: TodoExistValidationWorks -> PASS

--- R/Todo.Model.Validation.Service.R
+++ R/Todo.Model.Validation.Service.R
@@ -1,5 +1,12 @@
 Todo.Model.Validation.Service <- \(){
+  exception <- Todo.Model.Exceptions()
+  
   validators <- list()
+
+  validators[["TodoExist"]] <- \(todo) {
+    todo |> is.null() |> exception[["TodoIsNull"]]()
+    return(todo)
+  }
 
   return(validators)  
 }
