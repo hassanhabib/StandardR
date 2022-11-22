1b6c198d88149d2984e97199834e74b98cef4797
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-22 06:49:30 -0800

    PROCESSING: TodoProcessingServiceUpsertShouldAddTodoIfNotExist --> PASS

--- R/Todo.Processing.Service.R
+++ R/Todo.Processing.Service.R
@@ -1,8 +1,15 @@
-Todo.Processing.Service <- \(service){
+Todo.Processing.Service <- \(todo.service){
   service <- list()
 
   service[['Upsert']] <- \(todo) {
+    todo.count <- 
+      todo[['Id']] |> 
+      todo.service[['RetrieveById']]() |> 
+      nrow()
     
+    if (todo.count == 0) {
+      todo |> todo.service[['Add']]()
+    }
   }
 
   return(service)
